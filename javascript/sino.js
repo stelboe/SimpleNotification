360c5becea61523ba8a55ba718e0de2d1a735782
stelboe <stelboe@gmail.com>
2015-07-27 13:22:53 +0200

    added extended sino

--- javascript/sino.js
+++ javascript/sino.js
@@ -1,20 +1,33 @@
 var timer = 0;
 function sino(content, attr, time) {
     (time == null) ? timer = 5000 : timer = time;
-    if ($('.sino').is(":visible")) {
+    if ($('.sino, .sinoext').is(":visible")) {
         console.log("there is already one sino existing");
     } else {
-        $('.sino').remove();
-        $('body').append("<div class='sino " + attr + "'><div class='content'>" + content + "</div> <div class='control'> x</div></div>");
+        $('.sino, .sinoext').remove();
+        $('body').append("<div class='sino " + attr + "'><div class='content'>"+ content + "</div><div class='control'> x</div></div>");
         $('.sino').slideToggle();
     }
-    $('.control').click(function () {
-        console.log("click");
-        $(".sino").fadeOut("slow");
-    });
-    if ($('.sino').hasClass("timed")) {
-        setTimeout(function () {
-            $('.sino').fadeOut('slow');
-        }, time);
+        $('.control').click(function () {
+            $(".sino").fadeOut("slow");
+        });
+        if ($('.sino').hasClass("timed")) {
+            setTimeout(function () {
+                $('.sino').fadeOut('slow');
+            }, time);
+        }
+}
+function sinoext(header, content, choice1, choice2, attr){
+    if ($('.sino, .sinoext').is(":visible")) {
+        console.log("there is already one sino existing");
+    } else {
+        $('.sino, .sinoext').remove();
+        $('body').append('<div class="sinoext '+attr+'"><div class="header"><div class="header_text">'+header+'</div></div>' +
+        '<div class="content">'+content+'</div><div class="footer"><div class="left">'+choice1+'</div>' +
+        '<div class="right">'+choice2+'</div></div></div>');
+        $('.sinoext').slideToggle();
     }
+    $('.footer').click(function () {
+        $(".sinoext").fadeOut("slow");
+    });
 }
